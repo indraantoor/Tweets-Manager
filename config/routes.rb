857eb893fbacad9bc528c68a9dee5a2ae4e3a3a3
indraantoor <indraantoor@gmail.com>
2022-05-15 15:11:33 +0530

    Add: signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #GET /about
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "main#index"
