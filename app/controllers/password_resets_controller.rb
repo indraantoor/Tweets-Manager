d491362f894b43f6995e4dcb8aa2cf3e0d2d35ca
indraantoor <indraantoor@gmail.com>
2022-05-19 22:46:08 +0530

    Add: forgot password form

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,14 @@
+class PasswordResetsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+           PasswordMailer.with(user: @user).reset.deliver_now
+        else
+            redirect_to root_path, notice: "If account exists, we will send steps to reset your password on the mail."
+        end  
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   
   delete "logout", to: "sessions#destroy"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "main#index"
