94744552b283377ed5c5c7f6ea04cec0fee36675
indraantoor <indraantoor@gmail.com>
2022-05-19 23:34:38 +0530

    Feat: password reset using mail sent to user's email

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,9 +7,29 @@ class PasswordResetsController < ApplicationController
 
         if @user.present?
            PasswordMailer.with(user: @user).reset.deliver_now
-           redirect_to root_path, notice: "If account exists, we will send steps to reset your password on the mail."
-        else
-            redirect_to root_path, notice: "If account exists, we will send steps to reset your password on the mail."
         end  
+    
+        redirect_to root_path, notice: "If account exists, we will send steps to reset your password on the mail."
     end
+
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        rescue ActiveSupport::MessageVerifier::InvalidSignature
+            redirect_to sign_in_path, alert: "Link expired try again."
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+        else
+            render :edit
+        end
+    end
+
+    private
+
+        def password_params
+            params.require(:user).permit(:password, :password_confirmation)
+        end
 end
