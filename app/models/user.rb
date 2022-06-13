36b060a88380391205081d8113f61e306cd70372
indraantoor <indraantoor@gmail.com>
2022-06-13 22:08:10 +0530

    Feat: connect twiiter account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@
 # --
 
 class User < ApplicationRecord
+    has_many :twitter_accounts
+    
     has_secure_password
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
