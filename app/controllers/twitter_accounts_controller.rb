16f3be939607e9b2b8bd66cc861ce1cf52760917
indraantoor <indraantoor@gmail.com>
2022-06-14 00:12:05 +0530

    Add: create a tweet form

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,13 +1,22 @@
 class TwitterAccountsController < ApplicationController
-    before_action :require_user_logged_in
+    before_action :require_user_logged_in 
+    before_action :set_twitter_account, only: [:show, :destroy]
     
     def index
         @twitter_accounts = Current.user.twitter_accounts
     end
 
+    def show
+    end
+
     def destroy
-        @twitter_account = Current.user.twitter_accounts.find(params[:id])
         @twitter_account.destroy
         redirect_to twitter_accounts_path, notice: "Successfully disconnected!"
     end
+
+    private
+
+        def set_twitter_account
+            @twitter_account = Current.user.twitter_accounts.find(params[:id])
+        end
 end
